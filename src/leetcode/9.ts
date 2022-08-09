8f5bb41f82119a3bd5067c2a72267c5def85a1d4
kongsa-0721 <ks210220364@icloud.com>
2022-08-09 22:07:08 +0800

    [update] update separator style

--- src/leetcode/9.ts
+++ src/leetcode/9.ts
@@ -1,1 +1,14 @@
-/** * 给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。 * 回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。 */function isPalindrome(x: number): boolean {  const str = x + "";  //const str1 = x.toString();  for (let i = 0; i < str.length; i++) {    if (str[i] !== str[str.length - i - 1]) {      return false;    }  }  return true;}
+/**
+ * 给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+ * 回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ */
+function isPalindrome(x: number): boolean {
+  const str = x + "";
+  //const str1 = x.toString();
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== str[str.length - i - 1]) {
+      return false;
+    }
+  }
+  return true;
+}
