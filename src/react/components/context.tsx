2f888d5ac6f6fbe6e6fcaf30ae87eb611790859e
kongsa-0721 <ks210220364@icloud.com>
2022-08-13 22:04:34 +0800

    [context] add context provider consumer

---
+++ src/react/components/context.tsx
@@ -0,0 +1,54 @@
+/**
+ * Created by KongSa on 2022/8/13-9:09 PM.
+ */
+import React from "react";
+//创建一个上下文 每一个consumer都必须在这个provider里面 才可以消费这个provider
+const Context1 = React.createContext({});
+
+class ContextComponent extends React.Component<any, any> {
+  state = { name: "kongsa" };
+  handleChange = () => {
+    this.setState({ name: this.state.name + "!" });
+  };
+  render() {
+    return (
+      <Context1.Provider value={{ name: this.state.name }}>
+        {this.props.children}
+        <button onClick={this.handleChange}>点击</button>
+      </Context1.Provider>
+    );
+  }
+}
+
+class Item1 extends React.Component<any, any> {
+  render() {
+    return (
+      <Context1.Consumer>
+        {(value: any) => {
+          console.log(value);
+          return <>{value.name}</>;
+        }}
+      </Context1.Consumer>
+    );
+  }
+}
+
+class Item2 extends React.Component<any, any> {
+  render() {
+    return (
+      <Context1.Consumer>
+        {(value: any) => {
+          console.log(value);
+          return <>{value.name}</>;
+        }}
+      </Context1.Consumer>
+    );
+  }
+}
+
+// <ContextComponent>
+//   <Item1 />
+//   <Item2 />
+// </ContextComponent>;
+
+export { ContextComponent, Item1, Item2 };
