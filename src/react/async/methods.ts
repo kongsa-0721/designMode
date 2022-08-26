82f51a786c4d97829bd5c043d2455ea369b8da29
kongsa-0721 <ks210220364@icloud.com>
2022-08-26 18:02:28 +0800

    [update] first hack commit in 0826

--- src/react/async/methods.ts
+++ src/react/async/methods.ts
@@ -1,39 +1,72 @@
 /**
  * Created by KongSa on 2022/8/24-10:08 AM.
  */
-const p1 = Promise.resolve(1);
-const p2 = Promise.resolve(2);
-const p3 = Promise.reject(3);
+// @ts-nocheck
+// const p1 = Promise.resolve(1);
+// const p2 = Promise.resolve(2);
+// const p3 = Promise.reject(3);
 
 export function noSync() {
-  //all 有一个拒绝 直接拒绝 全部通过 返回一个数组
-  Promise.all([p1, p2, p3])
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((e) => {
-      console.log(e);
-    });
-  //全部落地之后的 返回状态以及返回值
-  Promise.allSettled([p1, p2, p3])
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((e) => {
-      console.log(e);
-    });
-  //返回最快的那个 成功用then接收 失败用catch
-  Promise.race([p3, p2, p1])
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((e) => {
-      console.log(e);
-    });
-  //成功或者失败都会执行 但是不能接收任何参数
-  Promise.resolve(3).finally(() => {
-    console.log("finally");
-  });
+  // //all 有一个拒绝 直接拒绝 全部通过 返回一个数组
+  // Promise.all([p1, p2, p3])
+  //   .then((res) => {
+  //     console.log(res);
+  //   })
+  //   .catch((e) => {
+  //     console.log(e);
+  //   });
+  // //全部落地之后的 返回状态以及返回值
+  // Promise.allSettled([p1, p2, p3])
+  //   .then((res) => {
+  //     console.log(res);
+  //   })
+  //   .catch((e) => {
+  //     console.log(e);
+  //   });
+  // //返回最快的那个 成功用then接收 失败用catch
+  // Promise.race([p3, p2, p1])
+  //   .then((res) => {
+  //     console.log(res);
+  //   })
+  //   .catch((e) => {
+  //     console.log(e);
+  //   });
+  // //成功或者失败都会执行 但是不能接收任何参数 返回一个设置了finally回调函数的promise对象
+  // Promise.resolve(3).finally(() => {
+  //   console.log("finally");
+  // });
+  function memorise(fn: any, resolver: any) {
+    let cache = new Map();
+    return function () {
+      const cacheKey = resolver
+        ? resolver(arguments)
+        : Array.from(arguments).join("_");
+      const contextMap = cache.get(cacheKey);
+      if (!contextMap) {
+        let value = fn.apply(this, arguments);
+        cache.set(cacheKey, new Map().set(this, value));
+        return value;
+      }
+      if (contextMap.has(this)) {
+        console.log("命中缓存");
+        return contextMap.get(this);
+      }
+      let val = fn.apply(this, arguments);
+      contextMap.set(this, val);
+      return val;
+    };
+  }
+
+  function add(a, b) {
+    return a + b;
+  }
+
+  const a = memorise(add);
+  //
+  // console.log(a(1, 2));
+  // console.log(a(1, 2));
+  // console.log(a(1, 23));
+  // console.log(a(1, 23));
 }
 
 class Commient {
@@ -121,6 +154,28 @@ class Commient {
           },
           (err) => {
             rej(err);
+          }
+        );
+      }
+    });
+  }
+  static allSettled(callback: any[]) {
+    return new Commient((resolve, reject) => {
+      let result: any[] = [];
+      let count = 0;
+      for (let i = 0; i < callback.length; i++) {
+        Promise.resolve(callback[i]).then(
+          (res) => {
+            result[i] = res;
+            if (++count === callback.length) {
+              resolve(result);
+            }
+          },
+          (error) => {
+            result[i] = error;
+            if (++count === callback.length) {
+              resolve(result);
+            }
           }
         );
       }
