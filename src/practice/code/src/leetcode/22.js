0b2e3f7dea2d038a0e83b92c4427bfe1c4af2d98
kongsa-0721 <ks210220364@icloud.com>
2022-10-01 20:35:52 +0800

    [leetcode22] dfs+剪枝

---
+++ src/practice/code/src/leetcode/22.js
@@ -0,0 +1,32 @@
+/**
+ * Created by KongSa on 2022/10/1-7:51 PM.
+ * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+ */
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+let generateParenthesis = function (n) {
+  if (n == 0) {
+    return [];
+  }
+  if (n === 1) {
+    return ["()"];
+  }
+  let res = [];
+  function dfs(left, right, str) {
+    if (left === n && right === n) {
+      res.push(str);
+      return;
+    }
+    //剪枝
+    if (left > n || left < right) {
+      return;
+    }
+    dfs(left + 1, right, str + "(");
+    dfs(left, right + 1, str + ")");
+  }
+  dfs(0, 0, "");
+  return res;
+};
+console.log(generateParenthesis(3));
