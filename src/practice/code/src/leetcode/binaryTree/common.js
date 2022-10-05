ab5de5ca550ac89b9777b7136c9f150ccda15c9f
kongsa-0721 <ks210220364@icloud.com>
2022-10-05 20:33:49 +0800

    [binaryTree] add some

---
+++ src/practice/code/src/leetcode/binaryTree/common.js
@@ -0,0 +1,34 @@
+/**
+ * Created by KongSa on 2022/10/5-4:34 PM.
+ */
+class TreeNode {
+  constructor(value) {
+    this.val = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(values) {
+    const queue = [this];
+    let i = 0;
+    while (queue.length > 0) {
+      let current = queue.shift();
+      for (let side of ["left", "right"]) {
+        if (!current[side]) {
+          if (values[i] !== null) {
+            current[side] = new TreeNode(values[i]);
+          }
+          i++;
+          if (i >= values.length) return this;
+        }
+        if (current[side]) queue.push(current[side]);
+      }
+    }
+    return this;
+  }
+}
+
+// const tree = new TreeNode(15);
+// tree.insert([1, 2, 3, 4, 3, 8, 7, 5, 10, 12, 11, null, null, null]);
+
+export default TreeNode;
---
+++ src/practice/code/src/leetcode/binaryTree/pre.js
@@ -0,0 +1,43 @@
+/**
+ * Created by KongSa on 2022/10/5-4:43 PM.
+ */
+import TreeNode from "./common.js";
+const tree = new TreeNode(15);
+tree.insert([1, 2, 3, 4, 3, 8, 7, 5, 10, 12, 11, null, null, null]);
+
+//前序遍历
+let Traversal = function (root) {
+  return backTrack(root, []);
+};
+function backTrack(root, res) {
+  if (!root) return root;
+  res.push(root.val);
+  backTrack(root.left, res);
+  //中序
+  backTrack(root.right, res);
+  //后序
+  return res;
+}
+let arr = Traversal(tree).sort((a, b) => a - b);
+
+//练一下迭代 递归转迭代需要一个stack数组
+/**
+ * 根左右
+ * 左根右
+ * 左右根
+ * 迭代使用的是栈 所以顺序是反过来的
+ * @param root
+ */
+let preorderTraversal = function (root) {
+  let res = [];
+  if (!root) return res;
+  let stack = [root];
+  while (stack.length) {
+    //更通用的写法是放一个标识位置 null 根据这个null来判断
+    let node = stack.pop();
+    res.push(node.val);
+    node.right && stack.push(node.right);
+    node.left && stack.push(node.left);
+  }
+  return res;
+};
