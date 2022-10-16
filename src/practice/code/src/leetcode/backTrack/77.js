effcf50b86108612494fda5ccd7b966de0d69fe4
kongsa-0721 <ks210220364@icloud.com>
2022-10-16 15:29:24 +0800

    [leetcode] add some problems about backTrack

---
+++ src/practice/code/src/leetcode/backTrack/77.js
@@ -0,0 +1,30 @@
+/**
+ * Created by KongSa on 2022/10/16-3:24 PM.
+ *
+ * 给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合。
+ * 你可以按 任何顺序 返回答案。
+ */
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+let combine = function (n, k) {
+  let ret = [];
+  let excuting = new Array(n);
+  for (let i = 0; i < excuting.length; i++) {
+    excuting[i] = i + 1;
+  }
+  function backTrack(arr, index) {
+    if (arr.length === k) {
+      ret.push(arr.slice());
+    }
+    for (let i = index; i < excuting.length; i++) {
+      arr.push(excuting[i]);
+      backTrack(arr, i + 1);
+      arr.pop();
+    }
+  }
+  backTrack([], 0);
+  return ret;
+};
---
+++ src/practice/code/src/leetcode/backTrack/90.js
@@ -0,0 +1,33 @@
+/**
+ * Created by KongSa on 2022/10/16-2:59 PM.
+ *
+ * 子集 II
+ * 给你一个整数数组 nums ，其中可能包含重复元素，请你返回该数组所有可能的子集（幂集）。
+ * 解集 不能 包含重复的子集。返回的解集中，子集可以按 任意顺序 排列。
+ */
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+let subsetsWithDup = function (nums) {
+  let ret = [];
+  let set = new Set();
+  //可以先给它排序
+  nums = nums.sort((a, b) => a - b);
+  function backTrack(arr, index) {
+    // 使用set去重 set JSON.stringify
+    if (set.has(JSON.stringify(arr))) {
+      return;
+    } else {
+      set.add(JSON.stringify(arr));
+      ret.push(arr.slice());
+    }
+    for (let i = index; i < nums.length; i++) {
+      arr.push(nums[i]);
+      backTrack(arr, i + 1);
+      arr.pop();
+    }
+  }
+  backTrack([], 0);
+  return ret;
+};
