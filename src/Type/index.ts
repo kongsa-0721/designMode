9d8692ba9d56779502c596f8a34550fc471fcd3e
kongsa-0721 <ks020809@163.com>
2022-12-15 14:28:38 +0800

    [feat] update in mbp14

--- src/Type/index.ts
+++ src/Type/index.ts
@@ -2,11 +2,11 @@
  * Created by KongSa on 2022/4/14-8:54 PM.
  */
 
-import { getObjVal } from "./objType";
-import { cl1 } from "./classType";
+// import { getObjVal } from "./objType";
+// import { cl1 } from "./classType";
 
-console.log(getObjVal("name"));
-console.log(getObjVal("age"));
-new cl1({ name: "kongsa" });
+// console.log(getObjVal("name"));
+// console.log(getObjVal("age"));
+// new cl1({ name: "kongsa" });
 
 export {};
