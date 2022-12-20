f2897969f4ac9fc1ba01d41bde3c38793b6bceec
kongsa-0721 <ks210220364@icloud.com>
2022-12-20 21:31:48 +0800

    [feat] add

--- src/react/app.tsx
+++ src/react/app.tsx
@@ -7,17 +7,17 @@ import Combine from "./redux/combine";
 import { Protal } from "./basic/protal";
 import { Provider } from "react-redux";
 import { store } from "./redux/store/store";
-import { BasicAhooks } from "./ahooks/basic";
+import { Mom } from "./test";
 
 function App() {
-	return (
-		<Provider store={store}>
-			this is app component
-			<Combine />
-			<Protal />
-			<BasicAhooks />
-		</Provider>
-	);
+  return (
+    <Provider store={store}>
+      this is app component
+      <Combine />
+      <Mom />
+      <Protal />
+    </Provider>
+  );
 }
 
 export default App;
