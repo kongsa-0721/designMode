ce24218f7b410790e020999a2787211a5eea9828
kongsa-0721 <ks210220364@icloud.com>
2023-02-25 15:08:08 +0800

    [feat] use prettier

--- src/react/test.tsx
+++ src/react/test.tsx
@@ -2,9 +2,9 @@ import moment from "moment";
 import React from "react";
 
 function Mom() {
-  const value = new Date().getTime();
-  const mo = moment(value).format("yyyy-MM-DD_HH:mm:ss");
-  return <>{mo}</>;
+	const value = new Date().getTime();
+	const mo = moment(value).format("yyyy-MM-DD_HH:mm:ss");
+	return <>{mo}</>;
 }
 
 export { Mom };
