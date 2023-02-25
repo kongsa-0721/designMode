6c5438a536d9f5d10ad8ecebcefce9b06b53d293
kongsa-0721 <ks210220364@icloud.com>
2023-02-25 15:07:46 +0800

    [feat] add stash in mbp15

--- src/react/app.tsx
+++ src/react/app.tsx
@@ -8,14 +8,16 @@ import { Protal } from "./basic/protal";
 import { Provider } from "react-redux";
 import { store } from "./redux/store/store";
 import { BasicAhooks } from "./ahooks/basic";
+import { Mom } from "./test";
 
 function App() {
 	return (
 		<Provider store={store}>
 			this is app component
 			<Combine />
-			<Protal />
+			<Mom />
 			<BasicAhooks />
+			<Protal />
 		</Provider>
 	);
 }
---
+++ src/react/test.tsx
@@ -0,0 +1,10 @@
+import moment from "moment";
+import React from "react";
+
+function Mom() {
+  const value = new Date().getTime();
+  const mo = moment(value).format("yyyy-MM-DD_HH:mm:ss");
+  return <>{mo}</>;
+}
+
+export { Mom };
